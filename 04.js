d43a1a0d9499e6190fe51fcb979e49f26fbc94ed
bhosale-ajay <bhosale_ajay@hotmail.com>
2016-01-07 23:18:30 +0530

    Updated ReadMe and Added Lib required for Day 4

--- 04.js
+++ 04.js
@@ -1,15 +1,27 @@
 //http://www.myersdaily.org/joseph/javascript/md5.js
 
-var input = 'yzbqklnj';
+//Goto page
+//http://www.myersdaily.org/joseph/javascript/md5-text.html
 
-function findNumber(input) {
-    for (var number = 999999; number < 9999999; number++) {
+var md5 = require("./lib/md5.js");
+
+var input = "yzbqklnj";
+
+function findNumber(input, guessFrom, guessTo, startsWith) {
+    var found = false;
+    var number = 0;
+    for (number = guessFrom; number < guessTo; number++) {
         var hash = md5(input + number);
-        if (hash.startsWith("000000")) {
-            console.log(number);
+        if (hash.startsWith(startsWith)) {
+            found = true;
             break;
         }
     }
+    console.log(found ? number : -1);
 }
 
-findNumber('yzbqklnj');
+//282749
+findNumber(input, 280000, 320000, "00000");
+//9962624
+findNumber(input, 8888888, 9999999, "000000");
+//if you are getting -1 or an incorrect answer, increase the span of guess
--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # Advent of Code -  Solutions
 All the solutions (except day 4), can be run via node command or copying the code to browser console.
 like
+
 `
-node.js 01.js
+node 01.js
 `
+
+Day 4 solution use 'require' and hence works only with node.
+
+##Notes
+1. Your inputs may differ.
+2. Day 15, takes bit longer to compute all the combinations.
+3. Most of the samples use ES6 and functional features of JavaScript, and they can be improved further.
+For any suggestions, bugs feel free to ping me [@bhosaleajay](https://twitter.com/bhosaleajay) or send a pull request. 
+
+##Lessons Learned
+1. Read the instructions carefully. I wasted most of the time, just because I didn't read the instructions properly.
+2. If the site is not accepting your answer for Part II, then it may possible that there is bug with Part I implementation (even if, it is giving a perfect answer).
