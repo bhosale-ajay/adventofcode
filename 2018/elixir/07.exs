8b5381dee1c869390de5ffb057be87c0fd19647c
Ajay Bhosale <bhosale_ajay@hotmail.com>
2018-12-07 23:14:13 -0500

    Day 7, elixir part 2

--- 2018/elixir/07.exs
+++ 2018/elixir/07.exs
@@ -13,12 +13,6 @@ build_graph = fn [_, p, c], [graph, dep] ->
   ]
 end
 
-find_root_nodes = fn [graph, dep] ->
-  graph
-  |> Map.keys()
-  |> Enum.reject(&(Map.has_key?(dep, &1)))
-end
-
 parse = fn ip ->
   ip
   |> get_content.()
@@ -26,7 +20,13 @@ parse = fn ip ->
   |> Enum.reduce([%{}, %{}], build_graph)
 end
 
-defmodule NP do
+defmodule D07 do
+  def find_root_nodes([graph, dep]) do
+    graph
+    |> Map.keys()
+    |> Enum.reject(&(Map.has_key?(dep, &1)))
+  end
+
   def get_ready_nodes(n, graph, dep) do
     with up_dep = Enum.reduce(graph[n], dep, fn c, up_dep -> Map.update(up_dep, c, 0, &(&1 - 1)) end),
          child_nodes = Enum.filter(graph[n], (&(up_dep[&1] === 0))) do
@@ -71,38 +71,46 @@ defmodule NP do
     |> Enum.take(count)
     |> Enum.map(fn n -> [n, base + :binary.first(n) - ?A + 1] end)
   end
+
   def process_steps([], [], _, _, step_count, _, _) do
     step_count
   end
+
   def process_steps(nodes, workers, graph, dep, step_count, worker_count, base) do
     with free_wc = worker_count - Enum.count(workers),
-          # create workers
-          updated_workers = (if free_wc > 0, do: (workers ++ create_workers(nodes, free_wc, base)), else: workers),
-          # debug
-          # line_status = Enum.reduce(updated_workers, "", fn [n, _], acc -> "#{acc}#{n}\t" end),
-          # reduce count
-          updated_workers = Enum.map(updated_workers, fn [n, c] -> [n, c - 1] end),
-          # for completed tasks get next nodes
-          [next_nodes, updated_dep] = fetch_next_nodes(updated_workers, graph, dep),
-          # mix them with remaining nodes waiting for workers and sort
-          next_nodes = Enum.sort(Enum.drop(nodes, free_wc) ++ next_nodes),
-          # drop completed workers
-          updated_workers = Enum.filter(updated_workers, fn [_, c] -> c > 0 end) do
-            # IO.puts("#{step_count}\t#{line_status}")
-            process_steps(next_nodes, updated_workers, graph, updated_dep, step_count + 1, worker_count, base)
-          end
+      # create workers
+      updated_workers = (if free_wc > 0, do: (workers ++ create_workers(nodes, free_wc, base)), else: workers),
+      # debug
+      # line_status = Enum.reduce(updated_workers, "", fn [n, _], acc -> "#{acc}#{n}\t" end),
+      # reduce count
+      updated_workers = Enum.map(updated_workers, fn [n, c] -> [n, c - 1] end),
+      # for completed tasks get next nodes
+      [next_nodes, updated_dep] = fetch_next_nodes(updated_workers, graph, dep),
+      # mix them with remaining nodes waiting for workers and sort
+      next_nodes = Enum.sort(Enum.drop(nodes, free_wc) ++ next_nodes),
+      # drop completed workers
+      updated_workers = Enum.filter(updated_workers, fn [_, c] -> c > 0 end) do
+        # IO.puts("#{step_count}\t#{line_status}")
+        process_steps(next_nodes, updated_workers, graph, updated_dep, step_count + 1, worker_count, base)
+    end
   end
-end
 
-determine_order = fn [graph, dep] -> NP.traverse_nodes(find_root_nodes.([graph, dep]), graph, dep, "") end
-count_steps = fn [graph, dep], worker_count, base -> NP.process_steps(find_root_nodes.([graph, dep]), [], graph, dep, 0, worker_count, base) end
+  def determine_order([graph, dep]) do
+    traverse_nodes(find_root_nodes([graph, dep]), graph, dep, "")
+  end
+
+  def count_steps([graph, dep], worker_count, base) do
+    process_steps(find_root_nodes([graph, dep]), [], graph, dep, 0, worker_count, base)
+  end
+
+end
 
 test_requirements = parse.("../inputs/07-test.txt")
 puzzle_requirements = parse.("../inputs/07.txt")
 
-"CABDFE" = determine_order.(test_requirements)
-"BDHNEGOLQASVWYPXUMZJIKRTFC" = determine_order.(puzzle_requirements)
-15 = count_steps.(test_requirements, 2, 0)
-1107 = count_steps.(puzzle_requirements, 5, 60)
+"CABDFE" = D07.determine_order(test_requirements)
+"BDHNEGOLQASVWYPXUMZJIKRTFC" = D07.determine_order(puzzle_requirements)
+15 = D07.count_steps(test_requirements, 2, 0)
+1107 = D07.count_steps(puzzle_requirements, 5, 60)
 
 IO.puts("Done")
