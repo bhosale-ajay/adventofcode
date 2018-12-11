6c58ee442b6da4eda6e5c849d221425a6cafbc94
Ajay Bhosale <bhosale_ajay@hotmail.com>
2018-12-10 19:57:13 -0500

    empty elixir solutions for Day 9 and 10

---
+++ 2018/elixir/09.exs
@@ -0,0 +1,2 @@
+# No solution for day 09, current knowlegde of elixir not enough to implement LinkedList or Zipper
+IO.puts("Not done")
---
+++ 2018/elixir/10.exs
@@ -0,0 +1,2 @@
+# No solution for day 10, did not found a way to clean console programmatically
+IO.puts("Not done")
