72921b30464f24b46c305b8ea9fc211215b68468
Ajay Bhosale <bhosale_ajay@hotmail.com>
2019-12-06 14:34:25 -0500

    House Keeping

--- 2019/ts/util.ts
+++ 2019/ts/util.ts
@@ -1,91 +1,104 @@
-import { readFileSync } from "fs";
+import { readFileSync } from 'fs';
 
 export interface Dictionary<T> {
-    [key: string]: T;
+  [key: string]: T;
 }
 
 export function getInput(day: string) {
-    return readFileSync(`../inputs/${day}.txt`, "utf8").replace(/\r/g, "");
+  return readFileSync(`../inputs/${day}.txt`, 'utf8').replace(/\r/g, '');
 }
 
-export function seenBefore<T, TKey = T>(seed: TKey[], keyMaker: (i: T) => TKey = _ => _ as unknown as TKey) {
-    const seen = new Set<TKey>(seed);
-    return (n: T) => {
-        const key = keyMaker(n);
-        if (seen.has(key)) {
-            return true;
-        } else {
-            seen.add(key);
-            return false;
-        }
-    };
-}
-
-export function getValue<T>(dictionary: Dictionary<T>, key: string, defaultValue: T): T {
-    if (dictionary[key] === undefined) {
-        dictionary[key] = defaultValue;
+export function seenBefore<T, TKey = T>(
+  seed: TKey[],
+  keyMaker: (i: T) => TKey = _ => (_ as unknown) as TKey
+) {
+  const seen = new Set<TKey>(seed);
+  return (n: T) => {
+    const key = keyMaker(n);
+    if (seen.has(key)) {
+      return true;
+    } else {
+      seen.add(key);
+      return false;
     }
-    return dictionary[key];
+  };
 }
 
-export function generate<T>(fromX: number, toX: number, fromY: number, toY: number, action: (x: number, y: number) => T) {
-    const result: T[] = [];
-    for (let x = fromX; x <= toX; x++) {
-        for (let y = fromY; y <= toY; y++) {
-            result.push(action(x, y));
-        }
+export function getValue<T>(
+  dictionary: Dictionary<T>,
+  key: string,
+  defaultValue: T
+): T {
+  if (dictionary[key] === undefined) {
+    dictionary[key] = defaultValue;
+  }
+  return dictionary[key];
+}
+
+export function generate<T>(
+  fromX: number,
+  toX: number,
+  fromY: number,
+  toY: number,
+  action: (x: number, y: number) => T
+) {
+  const result: T[] = [];
+  for (let x = fromX; x <= toX; x++) {
+    for (let y = fromY; y <= toY; y++) {
+      result.push(action(x, y));
     }
-    return result;
+  }
+  return result;
 }
 
 export interface Node<T> {
-    value: T;
-    prev: Node<T>;
-    next: Node<T>;
+  value: T;
+  prev: Node<T>;
+  next: Node<T>;
 }
 export class LinkedList<T> {
-    private length = 0;
+  private length = 0;
 
-    public get count() {
-        return this.length;
-    }
+  public get count() {
+    return this.length;
+  }
 
-    public init(value: T): Node<T> {
-        const node: any = {
-            value
-        };
-        node.prev = node;
-        node.next = node;
-        this.length = 1;
-        return node as Node<T>;
-    }
+  public init(value: T): Node<T> {
+    const node: any = {
+      value
+    };
+    node.prev = node;
+    node.next = node;
+    this.length = 1;
+    return node as Node<T>;
+  }
 
-    public remove(node: Node<T>) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-        (node as any).prev = null;
-        (node as any).next = null;
-        this.length--;
-    }
+  public remove(node: Node<T>) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+    (node as any).prev = null;
+    (node as any).next = null;
+    this.length--;
+  }
 
-    public insertAfter(node: any, value: T) {
-        const currentNext = node.next;
-        const newNode = {
-            value,
-            prev: node,
-            next: currentNext
-        };
-        node.next = newNode;
-        currentNext.prev = newNode;
-        this.length++;
-        return newNode;
-    }
+  public insertAfter(node: any, value: T) {
+    const currentNext = node.next;
+    const newNode = {
+      value,
+      prev: node,
+      next: currentNext
+    };
+    node.next = newNode;
+    currentNext.prev = newNode;
+    this.length++;
+    return newNode;
+  }
 }
 
 export function sumIt(source: Iterable<number>) {
-    let result = 0;
-    for (const item of source) {
-        result = result + item;
-    }
-    return result;
+  let result = 0;
+  for (const item of source) {
+    result = result + item;
+  }
+  return result;
 }
