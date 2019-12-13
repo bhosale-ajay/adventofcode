7036a87966d2d0d6c9e357ffa25b88e77cef785b
Ajay Bhosale <bhosale_ajay@hotmail.com>
2019-12-13 10:24:16 -0500

    2019 Extracted IntCode

--- 2019/ts/D11.test.ts
+++ 2019/ts/D11.test.ts
@@ -1,6 +1,6 @@
 import { getInput, Dictionary } from './util';
+import { IntCodeComputer, SignalType } from './intcode';
 
-const parse = (s: string) => s.split(',').map(w => +w);
 const puzzleInput = getInput('11');
 const [MIN, MAX] = [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER];
 const [rx, rY, iX, iY, kX, kY] = [-1, -1, 1, 1, 0, 0];
@@ -16,35 +16,19 @@ enum OutputTurn {
 }
 const keyMaker = (x: number, y: number) => `K${x}_${y}`;
 const paintProgram = (startWith: 0 | 1): [Dictionary<number>, number[]] => {
-  const p = parse(puzzleInput);
+  const computer = IntCodeComputer(puzzleInput);
   const panel: Dictionary<number> = {};
   let [x, y, dir] = [0, 0, U];
   let [minX, minY, maxX, maxY] = [MAX, MAX, MIN, MIN];
-  let rb = 0;
   let turn = OutputTurn.Paint;
   panel[keyMaker(x, y)] = startWith;
-  const gv = (ip: number) => (p[ip] === undefined ? 0 : p[ip]);
-  // prettier-ignore
-  const gi = (m: number, ip: number) => m === 0 ? p[ip] : m === 1 ? ip : rb + p[ip];
-  for (let ip = 0; ip < p.length; ) {
-    const i = p[ip];
-    const [_, tpm, spm, fpm] = (100000 + i).toString().split('');
-    const a = gi(+fpm, ip + 1);
-    const b = gi(+spm, ip + 2);
-    const c = gi(+tpm, ip + 3);
-    const opCode = i % 100;
-    if (opCode === 1) {
-      p[c] = gv(a) + gv(b);
-      ip = ip + 4;
-    } else if (opCode === 2) {
-      p[c] = gv(a) * gv(b);
-      ip = ip + 4;
-    } else if (opCode === 3) {
-      const v = panel[keyMaker(x, y)];
-      p[a] = v !== undefined ? v : 0;
-      ip = ip + 2;
-    } else if (opCode === 4) {
-      const output = gv(a);
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const currentPaint = panel[keyMaker(x, y)];
+    const input = currentPaint !== undefined ? currentPaint : 0;
+    const { value } = computer.next(input);
+    if (value.type === SignalType.OUTPUT) {
+      const output = value.number;
       if (turn === OutputTurn.Paint) {
         panel[keyMaker(x, y)] = output;
         turn = OutputTurn.Direction;
@@ -59,30 +43,14 @@ const paintProgram = (startWith: 0 | 1): [Dictionary<number>, number[]] => {
         dir = nd;
         turn = OutputTurn.Paint;
       }
-      ip = ip + 2;
-    } else if (opCode === 5) {
-      ip = gv(a) !== 0 ? gv(b) : ip + 3;
-    } else if (opCode === 6) {
-      ip = gv(a) === 0 ? gv(b) : ip + 3;
-    } else if (opCode === 7) {
-      p[c] = gv(a) < gv(b) ? 1 : 0;
-      ip = ip + 4;
-    } else if (opCode === 8) {
-      p[c] = gv(a) === gv(b) ? 1 : 0;
-      ip = ip + 4;
-    } else if (opCode === 9) {
-      rb = rb + gv(a);
-      ip = ip + 2;
-    } else if (opCode === 99) {
-      return [panel, [minX, minY, maxX, maxY]];
-    } else {
-      return [panel, [minX, minY, maxX, maxY]];
+    } else if (value.type === SignalType.COMPLETE) {
+      break;
     }
   }
   return [panel, [minX, minY, maxX, maxY]];
 };
 
-const renderNumber = (
+const displayNumber = (
   panel: Dictionary<number>,
   [x1, y1, x2, y2]: number[]
 ) => {
@@ -100,5 +68,5 @@ test('11', () => {
   const [p1] = paintProgram(0);
   expect(Object.keys(p1).length).toEqual(1863);
   const [p2, rect] = paintProgram(1);
-  renderNumber(p2, rect);
+  displayNumber(p2, rect);
 });
---
+++ 2019/ts/intcode.ts
@@ -0,0 +1,86 @@
+const parse = (s: string) => s.split(',').map(w => +w);
+
+export enum SignalType {
+  READY = 'READY',
+  INPUT = 'INPUT',
+  OUTPUT = 'OUTPUT',
+  COMPLETE = 'COMPLETE'
+}
+
+export class ReadySignal {
+  readonly type = SignalType.READY;
+}
+
+export class InputSignal {
+  readonly type = SignalType.INPUT;
+}
+
+export class OutputSignal {
+  readonly type = SignalType.OUTPUT;
+  constructor(readonly number: number) {}
+}
+
+export class CompleteSignal {
+  readonly type = SignalType.COMPLETE;
+}
+
+export type Message = InputSignal | OutputSignal | ReadySignal | CompleteSignal;
+
+type Options = {
+  overRideZeroLocation?: number;
+};
+
+export function* IntCodeComputer(
+  program: string,
+  { overRideZeroLocation }: Options = {}
+): Generator<Message, Message, number> {
+  const p = parse(program);
+  if (overRideZeroLocation !== undefined) {
+    p[0] = overRideZeroLocation;
+  }
+  yield new ReadySignal();
+  let rb = 0;
+  const gv = (ip: number) => (p[ip] === undefined ? 0 : p[ip]);
+  // prettier-ignore
+  const gi = (m: number, ip: number) => m === 0 ? p[ip] : m === 1 ? ip : rb + p[ip];
+  for (let ip = 0; ip < p.length; ) {
+    const i = p[ip];
+    const [_, tpm, spm, fpm] = (100000 + i).toString().split('');
+    const a = gi(+fpm, ip + 1);
+    const b = gi(+spm, ip + 2);
+    const c = gi(+tpm, ip + 3);
+    const opCode = i % 100;
+    if (opCode === 1) {
+      p[c] = gv(a) + gv(b);
+      ip = ip + 4;
+    } else if (opCode === 2) {
+      p[c] = gv(a) * gv(b);
+      ip = ip + 4;
+    } else if (opCode === 3) {
+      // eslint-disable-next-line require-atomic-updates
+      p[a] = yield new InputSignal();
+      ip = ip + 2;
+    } else if (opCode === 4) {
+      yield new OutputSignal(gv(a));
+      ip = ip + 2;
+    } else if (opCode === 5) {
+      ip = gv(a) !== 0 ? gv(b) : ip + 3;
+    } else if (opCode === 6) {
+      ip = gv(a) === 0 ? gv(b) : ip + 3;
+    } else if (opCode === 7) {
+      p[c] = gv(a) < gv(b) ? 1 : 0;
+      ip = ip + 4;
+    } else if (opCode === 8) {
+      p[c] = gv(a) === gv(b) ? 1 : 0;
+      ip = ip + 4;
+    } else if (opCode === 9) {
+      rb = rb + gv(a);
+      ip = ip + 2;
+    } else if (opCode === 99) {
+      break;
+    } else {
+      break;
+    }
+  }
+  return new CompleteSignal();
+}
