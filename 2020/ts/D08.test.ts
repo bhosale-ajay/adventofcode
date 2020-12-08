4a227e6b5eea49de2db05fe3ae3332b81f7fca27
Ajay Bhosale <bhosale_ajay@hotmail.com>
2020-12-08 18:41:18 -0500

    2020 Day 08 TS : Few Improvements

--- 2020/ts/D08.test.ts
+++ 2020/ts/D08.test.ts
@@ -1,33 +1,49 @@
+import { count } from 'dotless';
 import { mapLine } from './util';
 
 type State = [acc: number, current: number];
-type Instruction = (state: State, parameter: number) => State;
+type Executor = (state: State, parameter: number) => State;
+type Instruction = [executor: Executor, fix: Executor | undefined];
 type Line = [instruction: Instruction, parameter: number];
 type Program = Line[];
-const nop = ([a, c]: State, _: number): State => [a, c + 1];
-const acc = ([a, c]: State, p: number): State => [a + p, c + 1];
-const jmp = ([a, c]: State, p: number): State => [a, c + p];
+const nopExecutor = ([a, c]: State, _: number): State => [a, c + 1];
+const accExecutor = ([a, c]: State, p: number): State => [a + p, c + 1];
+const jmpExecutor = ([a, c]: State, p: number): State => [a, c + p];
+const nop = [nopExecutor, jmpExecutor] as Instruction;
+const acc = [accExecutor, undefined] as Instruction;
+const jmp = [jmpExecutor, nopExecutor] as Instruction;
 const ins: any = { nop, acc, jmp };
 const parseLine = (l: string) => [ins[l.substr(0, 3)], +l.substr(4)] as Line;
 const parse = (ip: string) => mapLine(ip, parseLine);
 
-const execute = (program: Program): [number, boolean] => {
+const execute = (program: Program, tryIndex = -1): [number, boolean] => {
     const seen = new Set<number>();
     let accumulator = 0,
-        current = 0;
+        current = 0,
+        previousInstruction = 0,
+        fixableInstructionIndex = 0;
     while (current < program.length) {
         seen.add(current);
-        const [instruction, parameter] = program[current];
-        const [updatedAcc, next] = instruction(
+        const [[executor, fix], parameter] = program[current];
+        let selectedExecutor = executor;
+        if (fix !== undefined) {
+            if (fixableInstructionIndex === tryIndex) {
+                selectedExecutor = fix;
+            }
+            fixableInstructionIndex = fixableInstructionIndex + 1;
+        }
+        const [updatedAcc, next] = selectedExecutor(
             [accumulator, current],
             parameter
         );
         if (seen.has(next)) {
             return [accumulator, false];
         }
+        previousInstruction = current;
         [accumulator, current] = [updatedAcc, next];
     }
-    return [accumulator, true];
+    const lastInstructionExecuted = program.length - previousInstruction === 1;
+    return [accumulator, lastInstructionExecuted];
 };
 
 const findAccumulator = (program: Program) => execute(program)[0];
@@ -35,27 +51,18 @@ const findAccumulator = (program: Program) => execute(program)[0];
 const fixProgram = (program: Program) => {
     let result = -1,
         success = false;
-    for (let i = 0; i < program.length && !success; i++) {
-        const instruction = program[i][0];
-        if (instruction === acc) {
-            continue;
-        }
-        program[i][0] = instruction === jmp ? nop : jmp;
-        [result, success] = execute(program);
-        program[i][0] = instruction;
+    const fixable = count<Line>(l => l[0][1] !== undefined)(program);
+    for (let i = 0; i < fixable && !success; i++) {
+        [result, success] = execute(program, i);
     }
     return result;
 };
 
-const testInput = parse('08-test');
-const input = parse('08');
-
-test('08, Part 1', () => {
+test('08', () => {
+    const testInput = parse('08-test');
+    const input = parse('08');
     expect(findAccumulator(testInput)).toEqual(5);
     expect(findAccumulator(input)).toEqual(1451);
-});
-
-test('08, Part 2', () => {
     expect(fixProgram(testInput)).toEqual(8);
     expect(fixProgram(input)).toEqual(1160);
 });
