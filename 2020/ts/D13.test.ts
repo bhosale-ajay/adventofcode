a1c488eb8e5a7815a6a14baaccddf9a99f1a0ad0
Ajay Bhosale <bhosale_ajay@hotmail.com>
2020-12-13 11:40:34 -0500

    2020 Day 10 TS Simplified

--- 2020/ts/D13.test.ts
+++ 2020/ts/D13.test.ts
@@ -29,51 +29,17 @@ const findEarliestBus = ([ts, buses]: Input) => {
     return earliestBus * waitTime;
 };
 
-// solution from @caderek
-// https://github.com/caderek/aoc2020/blob/main/src/day13/index.ts
-const gcd = (a: bigint, b: bigint) => {
-    let x = 1n,
-        y = 0n,
-        r = 0n,
-        s = 1n;
-
-    while (b !== 0n) {
-        const c = a % b;
-        const q = a / b;
-        a = b;
-        b = c;
-
-        const rPrim = r;
-        const sPrim = s;
-        r = x - q * r;
-        s = y - q * s;
-        x = rPrim;
-        y = sPrim;
+export const winGoldCoin = (buses: Bus[]): number => {
+    let time = buses[0][0],
+        step = buses[0][0];
+    for (let n = 1; n < buses.length; n++) {
+        const [busId, index] = buses[n];
+        while ((time + index) % busId !== 0) {
+            time += step;
+        }
+        step = step * busId;
     }
-    return x;
-};
-
-const mod = (a: bigint, b: bigint) => {
-    const x = a % b;
-    return x < 0n ? x + b : x;
-};
-
-const convertToBigNum = ([busId, busIndex]: Bus): [bigint, bigint] => {
-    const b = BigInt(busId);
-    return [BigInt(b), b - (BigInt(busIndex) % b)];
-};
-
-const winGoldCoin = (buses: Bus[]) => {
-    const bigBuses = buses.map(convertToBigNum);
-    const product = bigBuses.reduce((acc, [b]) => acc * b, 1n);
-    const factor = bigBuses
-        .map(([busId, busIndex]) => {
-            const N = product / busId;
-            const M = gcd(N, busId);
-            return busIndex * N * M;
-        })
-        .reduce((a, b) => a + b);
-    return mod(factor, product);
+    return time;
 };
 
 test('13', () => {
@@ -81,12 +47,12 @@ test('13', () => {
     const i = parse('13');
     expect(findEarliestBus(t)).toEqual(295);
     expect(findEarliestBus(i)).toEqual(174);
-    expect(winGoldCoin(t[1])).toEqual(1068781n);
-    expect(winGoldCoin(i[1])).toEqual(780601154795940n);
-    expect(winGoldCoin(parseBuses('3,5,7'))).toEqual(54n);
-    expect(winGoldCoin(parseBuses('17,x,13,19'))).toEqual(3417n);
-    expect(winGoldCoin(parseBuses('67,7,59,61'))).toEqual(754018n);
-    expect(winGoldCoin(parseBuses('67,x,7,59,61'))).toEqual(779210n);
-    expect(winGoldCoin(parseBuses('67,7,x,59,61'))).toEqual(1261476n);
-    expect(winGoldCoin(parseBuses('1789,37,47,1889'))).toEqual(1202161486n);
+    expect(winGoldCoin(t[1])).toEqual(1068781);
+    expect(winGoldCoin(i[1])).toEqual(780601154795940);
+    expect(winGoldCoin(parseBuses('3,5,7'))).toEqual(54);
+    expect(winGoldCoin(parseBuses('17,x,13,19'))).toEqual(3417);
+    expect(winGoldCoin(parseBuses('67,7,59,61'))).toEqual(754018);
+    expect(winGoldCoin(parseBuses('67,x,7,59,61'))).toEqual(779210);
+    expect(winGoldCoin(parseBuses('67,7,x,59,61'))).toEqual(1261476);
+    expect(winGoldCoin(parseBuses('1789,37,47,1889'))).toEqual(1202161486);
 });
