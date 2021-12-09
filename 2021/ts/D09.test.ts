af6ae0d83242ed1d7faa86321d66eeae9f4777ca
Ajay Bhosale <bhosale_ajay@hotmail.com>
2021-12-09 10:13:11 -0500

    Day 09 TypeScript

---
+++ 2021/ts/D09.test.ts
@@ -0,0 +1,65 @@
+import { descendingBy } from 'dotless';
+import {
+    gridBoundChecker,
+    mapGrid,
+    mapLine,
+    neighborAddresses,
+    BoundChecker,
+    Grid,
+    GridLocation,
+} from './util';
+
+type Area = Grid<number>;
+const lineToPoints = (l: string) => l.split('').map(n => +n);
+
+const pointCheck = (
+    p: number,
+    pl: GridLocation,
+    ns: number[]
+): [number, GridLocation] => {
+    const isLow = ns.every(e => e > p);
+    return [isLow ? p + 1 : 0, pl];
+};
+
+function exploreBasin(
+    [ri, ci]: GridLocation,
+    isBasin: BoundChecker,
+    visited: Set<string>
+): number {
+    let basinSize = 1;
+    for (const np of neighborAddresses(ri, ci).filter(isBasin)) {
+        const locationKey = np.toString();
+        if (visited.has(locationKey)) {
+            continue;
+        }
+        visited.add(locationKey);
+        basinSize = basinSize + exploreBasin(np, isBasin, visited);
+    }
+    return basinSize;
+}
+
+const basinChecker =
+    (area: Area, withInBound: BoundChecker) => (l: GridLocation) =>
+        withInBound(l) && area[l[0]][l[1]] < 9;
+
+const solve = (fn: string) => {
+    const area = mapLine(fn, lineToPoints) as Area;
+    const riskyPoints = mapGrid(area, pointCheck).filter(([r]) => r > 0);
+    const isBasin = basinChecker(area, gridBoundChecker(area));
+    const visited = new Set<string>();
+    let sumOfRisks = 0;
+    const basinSizes: number[] = [];
+    for (const [risk, point] of riskyPoints) {
+        sumOfRisks = sumOfRisks + risk;
+        visited.add(point.toString());
+        basinSizes.push(exploreBasin(point, isBasin, visited));
+    }
+    basinSizes.sort(descendingBy());
+    const productOfBasinSize = basinSizes[0] * basinSizes[1] * basinSizes[2];
+    return [sumOfRisks, productOfBasinSize];
+};
+
+test('09', () => {
+    expect(solve('09-test')).toEqual([15, 1134]);
+    expect(solve('09')).toEqual([535, 1122700]);
+});
--- 2021/ts/util.ts
+++ 2021/ts/util.ts
@@ -21,3 +21,47 @@ export function mapLineToNumber(fileName: string, separator = '\n'): number[] {
         .split(separator)
         .map(n => +n);
 }
+
+export type Grid<T> = T[][];
+export type GridLocation = [row: number, column: number];
+export const neighborAddresses = (r: number, c: number): GridLocation[] => [
+    [r - 1, c], // Top
+    [r, c - 1], // Left
+    [r, c + 1], // Right
+    [r + 1, c], // Bottom
+];
+
+export function mapGrid<T, U>(
+    grid: Grid<T>,
+    mapper: (item: T, itemLocation: GridLocation, neighbors: T[]) => U
+): U[] {
+    const rowCount = grid.length;
+    const columnCount = grid[0].length;
+    const result: U[] = [];
+    const withInBound = ([nr, nc]: GridLocation) =>
+        0 <= nr && nr < rowCount && 0 <= nc && nc < columnCount;
+    for (let ri = 0; ri < rowCount; ri++) {
+        for (let ci = 0; ci < columnCount; ci++) {
+            result.push(
+                mapper(
+                    grid[ri][ci],
+                    [ri, ci],
+                    neighborAddresses(ri, ci)
+                        .filter(withInBound)
+                        .map(([nr, nc]) => grid[nr][nc])
+                )
+            );
+        }
+    }
+    return result;
+}
+
+export const gridBoundChecker = <T>(
+    grid: Grid<T>
+): ((l: GridLocation) => boolean) => {
+    const rc = grid.length;
+    const cc = rc > 0 ? grid[0].length : 0;
+    return ([ar, ac]: GridLocation) =>
+        0 <= ar && ar < rc && 0 <= ac && ac <= cc;
+};
+export type BoundChecker = ReturnType<typeof gridBoundChecker>;
