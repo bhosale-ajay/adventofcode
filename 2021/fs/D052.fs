c785b19eb25f01e2421c8dafccabab892b2f5aef
Ajay Bhosale <bhosale_ajay@hotmail.com>
2022-11-29 18:43:06 -0500

    D05 : Option 2, removed mutable, and faster solution

--- 2021/fs/D052.fs
+++ 2021/fs/D052.fs
@@ -4,57 +4,68 @@ open System
 open Xunit
 open Common
 
-let points x1 y1 x2 y2 = 
-    seq {
-        let xd = if x1 < x2 then 1 else -1
-        let yd = if y1 < y2 then 1 else -1
-        let mutable x = x1
-        let mutable y = y1
-        while x <> x2 || y <> y2 do
-            yield (sprintf "%d:%d" x y)
-            if x <> x2 then
-                x <- x + xd
-            if y <> y2 then
-                y <- y + yd
-        yield (sprintf "%d:%d" x y)
-    }
+type Point = string // Set of Point as int * int takes almost double the time 
 
-type Line = 
-    {
-        points : string list
-        isHoriOrVert : bool
-    }
-    static member FromString (l: string) =
-        let ps = l.Split ([|","; " -> "|], StringSplitOptions.None) |> Array.map int
-        {
-            points = points ps[0] ps[1] ps[2] ps[3] |> Seq.toList
-            isHoriOrVert = ps[0] = ps[2] || ps[1] = ps[3]
-        }
+type LineType =
+    | Horizontal
+    | Vertical
+    | Diagonal
 
-let countInteractions (visited: Set<string>, intersected : Set<string>) point =
+type Line = Set<Point> * LineType
+        
+let isDiagonal (_, lt) = lt <> Diagonal
+
+let toPoint (a, b) = sprintf "%d:%d" a b
+
+let sequneceToLine lt ps  : Line =
+    (ps |> Seq.map toPoint |> Set.ofSeq, lt)
+
+let buildLine x1 y1 x2 y2 = 
+    let xd = if x1 < x2 then 1 else -1
+    let yd = if y1 < y2 then 1 else -1
+    if x1 = x2 then 
+        seq { y1 .. yd .. y2 } 
+        |> Seq.map (fun y -> (x1, y))
+        |> sequneceToLine Horizontal
+    else if y1 = y2 then 
+        seq { x1 .. xd .. x2 } 
+        |> Seq.map (fun x -> (x, y1))
+        |> sequneceToLine Vertical
+    else
+        seq { x1 .. xd .. x2 } 
+        |> Seq.zip <| seq { y1 .. yd .. y2 } 
+        |> sequneceToLine Diagonal
+
+let parseLine (l: string) =
+    let ps = l.Split ([|","; " -> "|], StringSplitOptions.None) |> Array.map int
+    match ps with
+    | [|x1; y1; x2; y2|] -> Some (buildLine x1 y1 x2 y2)
+    | _ -> None
+
+let countInteractions (visited: Set<Point>, intersected : Set<Point>) point =
     match (visited.Contains(point), intersected.Contains(point)) with
     | false, _ -> visited.Add point, intersected
     | true, false -> (visited, intersected.Add point)
     | _ -> (visited, intersected)
 
-let countInteractionsForLine acc line =
-    line.points
+let countInteractionsForLine acc (points, _) =
+    points
     |> Seq.fold countInteractions acc
 
 let solve (fn: string) =
     let (hnvLines, diaLines) =
         fn
-        |> mapLines Line.FromString 
-        |> Array.partition (fun l -> l.isHoriOrVert)
+        |> mapLines parseLine
+        |> Array.choose id
+        |> Array.partition isDiagonal
 
-    let (v, i) = 
+    let a, i = 
         hnvLines
         |> Seq.fold countInteractionsForLine (Set.empty, Set.empty)
 
-    let (_, i') =
+    let _, i' = 
         diaLines
-        |> Seq.fold countInteractionsForLine (v, i)
-
+        |> Seq.fold countInteractionsForLine (a, i)
     (i.Count, i'.Count)
 
 [<Theory>]
