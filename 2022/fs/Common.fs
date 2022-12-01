9a7de340e09a22f5219f7bf9b43de12a7ed3a840
Ajay Bhosale <bhosale_ajay@hotmail.com>
2022-12-01 16:45:12 -0500

    F# Support Files

---
+++ 2022/fs/Common.fs
@@ -0,0 +1,19 @@
+﻿module Common
+
+open System.IO
+
+let basePath =
+    @"D:\Ajay\Projects\adventofcode\2022\inputs"
+
+let seqOfLines fileName =
+    seq {
+        yield! Path.Combine(basePath, $"{fileName}.txt")
+        |> File.ReadLines
+        yield "";
+    }
+
+let (|Integer|_|) (l : string) =
+    match System.Int32.TryParse l with
+    | (true, number) -> Some number
+    | (false, _)     -> None
+
