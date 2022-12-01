146387e10d7536bc1095f447e4c6709ea1cd3a25
Ajay Bhosale <bhosale_ajay@hotmail.com>
2022-12-01 16:45:37 -0500

    F# Day 01

---
+++ 2022/fs/D01.fs
@@ -0,0 +1,31 @@
+module D01
+
+open Common
+open Xunit
+
+let pickTop3 c (m1, m2, m3) = 
+    if m3 > c then (m1, m2, m3)
+    else if m2 > c then (m1, m2, c)
+    else if m1 > c then (m1, c, m2)
+    else (c, m1, m2)
+
+let folder (c , m) = function
+    | Integer n -> (c + n, m)
+    | _ -> (0, pickTop3 c m)
+
+let solve fileName = 
+    let (_, (m1, m2, m3)) =
+        fileName 
+        |> seqOfLines
+        |> Seq.fold folder (0, (0,0,0))
+    (m1, m1 + m2 + m3)
+
+[<Theory>]
+[<InlineData("01-test", 24000, 45000)>]
+[<InlineData("01", 68467, 203420)>]
+let test01 fn ep1 ep2  =
+    let (ap1, ap2) = solve fn
+    Assert.Equal(ep1, ap1)
+    Assert.Equal(ep2, ap2)
+
+
