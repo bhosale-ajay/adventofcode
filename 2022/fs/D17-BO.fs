62e20bb6addbd31d8993b41e2910a6c6736c1c56
Ajay Bhosale <bhosale_ajay@hotmail.com>
2023-01-08 09:20:08 -0500

    F# Day 17 - Faster P1 & P2

--- 2022/fs/D17-BO.fs
+++ 2022/fs/D17-BO.fs
@@ -4,7 +4,7 @@ open Common
 open Xunit
 open System
 
-type Tunnel = Map<int, int>
+type Tunnel = Map<int, int * int>
 
 type RockShape = (int * int) array
 
@@ -53,7 +53,7 @@ let moveWithAir =
 let addBlankLineIfNotPresent t rn =
     t |> Map.change rn (function
         | Some l -> Some l
-        | None -> Some emptySpace)
+        | None -> Some (emptySpace, 0))
 
 let addSpace (t: Tunnel) ch rh =
     [ch + 1 .. ch + 3 + rh]
@@ -61,44 +61,82 @@ let addSpace (t: Tunnel) ch rh =
 
 let printTunnel (t: Tunnel) h =
     [h .. -1 .. 0]
-    |> Seq.iter (fun rn -> printfn "%s" (Convert.ToString(t[rn], 2).Replace('1', '#').Replace('0', ' ')))
+    |> Seq.iter (fun rn -> printfn "%s" (Convert.ToString(fst t[rn], 2).Replace('1', '#').Replace('0', ' ')))
 
 let moveRock mover (rl, rh) = mover rl, rh
 
 let canMoveDown (t: Tunnel) (r : RockShape) h =
-     r |> Array.forall (fun (rl, rh) -> canMove t[h - 1 + rh] rl)
+     r |> Array.forall (fun (rl, rh) -> canMove (fst t[h - 1 + rh]) rl)
+
+let notePosition (t: Tunnel) (pm: Map<int, int list>) n =
+     pm |> Map.change (fst t[n]) (function
+        | Some l -> Some(n :: l)
+        | None -> Some [n])
+
+let noteDiff = function
+    | h :: tail -> tail |> List.map (fun l -> h - l)
+    | _ -> []
 
 let solve fn =
     let nextAirFlow = fn |> readFile |> Array.ofSeq |> getNextItem
     let nextRock = rocks |> getNextItem
-    let times = 2022
+    let times = 4000
+    let hightAt = Array.create (times + 1) 0
 
-    let rec drop (t : Tunnel) (r: RockShape) h =
+    let rec drop (t : Tunnel) (r: RockShape) h ri =
         let mover = moveWithAir[nextAirFlow()]
-        let canFlowWithAir = r |> Array.forall (fun (rl, rh) -> canMove t[h + rh] (mover rl))
+        let canFlowWithAir = r |> Array.forall (fun (rl, rh) -> canMove (fst t[h + rh]) (mover rl))
         let r = if canFlowWithAir then (r |> Array.map (moveRock mover)) else r
         if canMoveDown t r h then
-            drop t r (h - 1)
+            drop t r (h - 1) ri
         else
-            let t = r |> Seq.fold (fun t (rl, rh) -> t |> Map.add (h + rh) (t[h + rh] ||| rl)) t
+            let t = r |> Seq.fold (fun t (rl, rh) -> t |> Map.add (h + rh) (fst t[h + rh] ||| rl, ri)) t
             t, h + snd r[0]
 
     let rec watch (t: Tunnel) ri height =
         match ri <= times with
         | false ->
-            height
+            t, height
         | _ ->
             let rock = nextRock()
             let t = addSpace t height rock.Length
-            let (t, h) = drop t rock (height + 4)
-            let h = (max height h)
+            let (t, h) = drop t rock (height + 4) ri
+            let h = max height h
+            hightAt[ri] <- h
             watch t (ri + 1) h
 
-    watch ([0, baseLine] |> Map) 1 0
+    let (t, h) = watch ([0, (baseLine, 0)] |> Map) 1 0
+    let p1 = hightAt[2022]
+    let safeHeight = h - 100
+    let pm = [1 .. safeHeight] |> Seq.fold (notePosition t) Map.empty
+    let ph = // Pattern Height
+        [safeHeight .. -1 .. (safeHeight - 10)]
+        |> Seq.map (fun hi -> fst t[hi])
+        |> Seq.distinct
+        |> Seq.map (fun v -> pm[v] |> noteDiff |> Set.ofList)
+        |> Seq.reduce (Set.intersect)
+        |> Set.minElement
+    let tpsi = // Pattern Start At
+        [1 .. ph]
+        |> Seq.find(fun hi -> fst t[hi + 0] = fst t[hi + ph + 0] && 
+                              fst t[hi + 1] = fst t[hi + ph + 1] &&
+                              fst t[hi + 2] = fst t[hi + ph + 2] &&
+                              fst t[hi + 3] = fst t[hi + ph + 3] &&
+                              fst t[hi + 4] = fst t[hi + ph + 4])
+    let prePatternHeight = tpsi - 1
+    let rocksPrePattern = snd t[prePatternHeight]
+    let rocks4Pattern = (snd t[ph + prePatternHeight]) - rocksPrePattern
+    let heightCoveredByPattern = 1000000000000L - int64 rocksPrePattern
+    let repetitions = heightCoveredByPattern / int64 rocks4Pattern
+    let rocksAtEnd = (int) (heightCoveredByPattern % int64 rocks4Pattern)
+    let preAndPostHeight = hightAt[rocksPrePattern + rocksAtEnd]
+    let p2 = (repetitions * int64 ph) + int64 preAndPostHeight;
+    p1, p2
 
 [<Theory>]
-[<InlineData("17-test", 3068)>]
-[<InlineData("17", 3100)>]
-let test17BO fn ep1 =
-    let ap1 = solve fn
+[<InlineData("17-test", 3068, 1514285714288L)>]
+[<InlineData("17", 3100, 1540634005751L)>]
+let test17BO fn ep1 ep2 =
+    let (ap1, ap2) = solve fn
     Assert.Equal(ep1, ap1)
+    Assert.Equal(ep2, ap2)
